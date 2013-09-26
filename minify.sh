713ed84cfc9a142a0270bfaa632458327cd827fd
Andrew Clissold <andrewclissold@gmail.com>
2013-09-26 11:41:19 -0400

    minify.sh: set permissions after minifying

--- minify.sh
+++ minify.sh
@@ -6,9 +6,11 @@ do
     then
         filename=${file%.js}
         curl -X POST -s --data-urlencode input@$filename.js http://javascript-minifier.com/raw > $filename.min.js
+        chmod 644 $filename.min.js
     elif [[ "$file" == *".css" ]]
     then
         filename=${file%.css}
         curl -X POST -s --data-urlencode input@$filename.css http://cssminifier.com/raw > $filename.min.css
+        chmod 644 $filename.min.css
     fi
 done
